3e2d8b1c3db3b951f7d3f8d86f77547ed639ae3a
mayur-driod <settymayurk@gmail.com>
2025-03-13 21:14:43 +0530

    made login page

---
+++ Frontend/src/Pages/Login.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+
+function Login() {
+    const [formdata,setformdata] = useState({
+        email:"",
+        password:""
+    });
+
+    const handlesubmit = async (e) => {
+        e.preventdefault();
+        if(!formdata.email||!formdata.password){
+            alert("All the fields are required!")
+            return
+        }
+        try{
+            await axios.post('http://localhost:3000/api/login',formdata);
+            console.log("data sent successfully using axios for login!")
+        }
+        
+        catch(err){
+            console.log("There was an error while sending login from frontend!", err);
+        }
+    }
+
+
+  return (
+    <>
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={(e)=>handlesubmit(e)}>
+                <div>
+                    <label>Email</label>
+                    <input type="text" value={formdata.email} onChange={(e)=> setformdata({...formdata,email:e.target.value})} />
+                </div>
+                
+                <div>
+                    <label>Password</label>
+                    <input type="password" value={formdata.password} onChange={(e)=>setformdata({...formdata,password:e.target.value})} />
+                </div>
+            </form>
+        </div>    
+    </>
+  )
+}
+
+export default Login
