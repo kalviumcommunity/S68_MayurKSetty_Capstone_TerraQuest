d097e28c00e14f2cbf5e1930bf2ef74463e55fac
mayur-driod <settymayurk@gmail.com>
2025-04-15 11:22:19 +0530

    arranged footer ele

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -4,10 +4,7 @@ import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 function Footer() {
   return (
     <footer className="bg-[#333] text-[#f2f2f2] text-center px-4 py-5 font-sans border-t-2 border-[#444] w-screen absolute left-0">
-      <div className="max-w-screen-xl mx-auto flex flex-col items-center gap-2.5">
-        <p className="text-sm m-0">
-          © 2025 Mayur K Setty. All rights reserved.
-        </p>
+      <div className="max-w-screen-xl mx-auto flex flex-row justify-between items-center">
         <div className="flex gap-4">
           <a
             href="https://www.instagram.com/mayurksetty.photography"
@@ -28,7 +25,7 @@ function Footer() {
             <FaLinkedin />
           </a>
           <a
-            href="https://github.com/mayur-driod"
+            href="https://github.com/kalviumcommunity/S68_MayurKSetty_Capstone_TerraQuest.git"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="GitHub"
@@ -37,6 +34,8 @@ function Footer() {
             <FaGithub />
           </a>
         </div>
+        <p className="text-sm">© 2025 Mayur K Setty. All rights reserved.</p>
+        <p>🌏 TerraQuest - made with ❤️ in India</p>
       </div>
     </footer>
   );
