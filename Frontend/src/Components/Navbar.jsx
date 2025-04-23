3464c412fdc716c046c66ba0c401a2f2e783ca36
mayur-driod <settymayurk@gmail.com>
2025-04-23 19:30:04 +0530

    prettied

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { Menu, X } from "lucide-react"; 
+import { Menu, X } from "lucide-react";
 
 function Navbar() {
   const currentUser = useSelector((state) => state.user.currentUser);
